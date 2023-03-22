840da06f4921016e7bedfa68c03d6b5a92a11be8
gepinfo <gepinfo>
2023-03-22 14:08:11 +0000

    updated generated code

--- application/client/web/accountingtemplate/src/app/template/template.tsx
+++ application/client/web/accountingtemplate/src/app/template/template.tsx
@@ -919,6 +919,310 @@ Signup</button>
 </div>
 </section>
  
+            
+    <section id="main" className="rows ">
+<div id="template-i5iyaj" className="gjs-cell">
+<div id="template-igcy" className="gjs-row">
+<div id="template-ib6i" className="gjs-cell">
+<img id="template-iqvi" src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/SvgjsG1008.png" className=""/>
+</div>
+<div id="template-in5k" className="gjs-cell">
+<button id="template-iu3i" className="button btn btn-primary ">
+Login</button>
+<button id="template-ix8h" className="button btn btn-primary ">
+Signup</button>
+</div>
+</div>
+<div id="template-i5354" className="gjs-row">
+<div id="template-i4vxf" className="gjs-cell">
+<div id="template-iab2l" className="">
+Get paid faster 
+          </div>
+<div id="template-idz9yw" className="">
+and grow your
+          </div>
+<div id="template-i89oi" className="">
+Business
+            <br id="template-iajof" className="">
+</br>
+</div>
+<div id="template-iov3i" className="">
+Simple, Well-Designed, Desktop Accounting 
+          </div>
+<div id="template-im47e6" className="">
+Software for Freelancers and Small Businesses
+          </div>
+</div>
+<div id="template-icb7j" className="gjs-cell">
+<img id="template-itd7kj" src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Group%20620.png" className=""/>
+</div>
+</div>
+<div id="template-izw8dv" className="gjs-row">
+<div id="template-im5jqr" className="gjs-cell">
+<div id="template-i9lrsn" className="">
+Almost Everything You Need
+          </div>
+<div id="template-iqezye" className="">
+Feature packed to handle your accounting requirements.
+          </div>
+</div>
+</div>
+<div id="template-iwyp9d" className="gjs-row">
+<div id="template-ixyb4i" className="gjs-cell">
+<div id="template-imhtyn" className="gjs-row">
+<div id="template-ih3aky" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/alInvoicing.png" id="template-ixgahn" className=""/>
+<div id="template-itbfjf" className="">
+Invoicing
+              </div>
+<div id="template-ipk5la" className="">
+Draft professional invoices and export them using customizable templates.
+              </div>
+Learn More</div>
+<div id="template-ixygus" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Journal%20Entries.png" id="template-inva5k" className=""/>
+<div id="template-iam3fj" className="">
+Journal Entries
+              </div>
+<div id="template-i9v9y8" className="">
+Record ad-hoc accounting entries to make expense entries and balance your books.
+              </div>
+Learn More</div>
+<div id="template-ix4vwd" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/alPayments.png" id="template-ib756t" className=""/>
+<div id="template-iuf3rl" className="">
+Payments
+              </div>
+<div id="template-i7y5mm" className="">
+Record the transfer of funds from your customers, or to your suppliers.
+              </div>
+Learn More</div>
+</div>
+<div id="template-itavy4" className="gjs-row">
+<div id="template-i4ujo6" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Discounts.png" id="template-iydwlg" className=""/>
+<div id="template-i57a41" className="">
+Discounts
+              </div>
+<div id="template-imcc8l" className="">
+Apply item-wise discounts to your invoices and track them using separate discount accounts.
+              </div>
+Learn More</div>
+<div id="template-irae7c" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/alReports.png" id="template-if8a8b" className=""/>
+<div id="template-iyc8dt" className="">
+Reports
+              </div>
+<div id="template-igxkkb" className="">
+Keep track of your business' health using financial reports such as profit and loss, and balance sheet.
+              </div>
+Learn More</div>
+<div id="template-iv68cw" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/alQuick%20Search.png" id="template-i60b4a" className=""/>
+<div id="template-ixiduk" className="">
+Quick Search
+              </div>
+<div id="template-ikix0e" className="">
+Instantly find what you are looking for within nine keystrokes.
+              </div>
+Learn More</div>
+</div>
+<div id="template-itp7vx" className="gjs-row">
+<div id="template-iv3bgt" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Data%20Import.png" id="template-iabv1b" className=""/>
+<div id="template-ijmalv" className="">
+Data Import
+              </div>
+<div id="template-ilqd5v" className="">
+Import business data from other accounting software with ease using csv templates.
+              </div>
+Learn More</div>
+<div id="template-i9srmt" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Group%20942.png" id="template-iqs943" className=""/>
+<div id="template-ivc0rc" className="">
+Multi Currency Invoicing
+              </div>
+<div id="template-ikjzng" className="">
+Create and export invoices in your customer's currency.
+              </div>
+Learn More</div>
+<div id="template-i0jd7y" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Inventory%20Management.png" id="template-if4eya" className=""/>
+<div id="template-i4homk" className="">
+Inventory Management
+              </div>
+<div id="template-iojvum" className="">
+Track and manage the flow of items and their valuation.
+              </div>
+Learn More</div>
+</div>
+</div>
+</div>
+<div id="template-id6x33" className="gjs-row">
+<div id="template-icc9j6" className="gjs-cell">
+<div id="template-iuogiw" className="gjs-row">
+<div id="template-ikh81v" className="gjs-cell">
+<div id="template-ipscez" className="">
+Simple And Beautiful
+              </div>
+<div id="template-i6e1tf" className="">
+An intuitive interface you will love using
+              </div>
+</div>
+</div>
+<div id="template-iiu6fe" className="gjs-row">
+<div id="template-ihg6fk" className="gjs-cell">
+<div id="template-ibdwni" className="gjs-row">
+<div id="template-ib8qsr" className="gjs-cell">
+<div id="template-ih3107" className="gjs-row">
+<div id="template-ih28y1" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Invoicing.png" id="template-in06mx" className=""/>
+</div>
+</div>
+<div id="template-itzslh" className="">
+Invoicing
+                  </div>
+</div>
+<div id="template-ila50n" className="gjs-cell">
+<div id="template-inaa0h" className="gjs-row">
+<div id="template-igu6nq" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Payments.png" id="template-i5cjfk" className=""/>
+</div>
+</div>
+<div id="template-isk86c" className="">
+Payments
+                  </div>
+</div>
+<div id="template-i02wwu" className="gjs-cell">
+<div id="template-i8xskx" className="gjs-row">
+<div id="template-i5i8yw" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Ledger.png" id="template-i4h7qu" className=""/>
+</div>
+</div>
+<div id="template-ij9872" className="">
+Ledger
+                  </div>
+</div>
+<div id="template-i7urb8" className="gjs-cell">
+<div id="template-i92vok" className="gjs-row">
+<div id="template-isbjrz" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Reports.png" id="template-iweiv7" className=""/>
+</div>
+</div>
+<div id="template-iyn5gg" className="">
+Reports
+                  </div>
+</div>
+<div id="template-ie7r8g" className="gjs-cell">
+<div id="template-ira8ci" className="gjs-row">
+<div id="template-imrc8a" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Quick%20Search.png" id="template-ia4h8h" className=""/>
+</div>
+</div>
+<div id="template-i47kvy" className="">
+Quick Search
+                  </div>
+</div>
+<div id="template-ioru1f" className="gjs-cell">
+<div id="template-igqzrx" className="gjs-row">
+<div id="template-i8oij9" className="gjs-cell">
+<img src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/Customizable.png" id="template-ixgluo" className=""/>
+</div>
+</div>
+<div id="template-i5gizi" className="">
+Customizable
+                  </div>
+</div>
+</div>
+</div>
+<div id="template-isit6s" className="gjs-cell">
+<img id="template-i5ub0v" src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/newDashboard.png" className=""/>
+</div>
+</div>
+</div>
+</div>
+<div id="template-idysi1" className="gjs-row">
+<div id="template-inm53x" className="gjs-cell">
+<div id="template-iud7ue" className="gjs-row">
+<div id="template-injr1i" className="gjs-cell">
+<div id="template-ia1ol8" className="">
+Built On Principles
+              </div>
+<div id="template-iw0oeg" className="">
+Why 10decoders books stands apart
+              </div>
+</div>
+</div>
+<div id="template-ilt8lw" className="gjs-row">
+<div id="template-idav3k" className="gjs-cell">
+<div id="template-i866fe" className="">
+Why 10decoders books stands apart
+              </div>
+<div id="template-iuwgm8" className="">
+Draft professional invoices and export them using customizable templates.
+              </div>
+<img id="template-ikn3gw" src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/arrow.png" className=""/>
+</div>
+<div id="template-iqd77r" className="gjs-cell">
+<div id="template-ivh91h" className="">
+No Cloud
+              </div>
+<div id="template-i96q9k" className="">
+Record ad-hoc accounting entries to make expense entries and balance your books.
+              </div>
+<img id="template-iugny3" src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/arrow.png" className=""/>
+</div>
+</div>
+<div id="template-iw5syb" className="gjs-row">
+<div id="template-iwe14g" className="gjs-cell">
+<div id="template-iabm7p" className="">
+Privacy
+              </div>
+<div id="template-ifm4ha" className="">
+Apply item-wise discounts to your invoices and track them using separate discount accounts.
+              </div>
+<img id="template-iguzaf" src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/arrow.png" className=""/>
+</div>
+<div id="template-iytd57" className="gjs-cell">
+<div id="template-i3tnwf" className="">
+Free And Open Source
+              </div>
+<div id="template-in7q0y" className="">
+Keep track of your business' health using financial reports such as profit and loss, and balance sheet.
+              </div>
+<img id="template-iwknui" src="https://grapesjs-images.s3.amazonaws.com/grapesjsimages/arrow.png" className=""/>
+</div>
+</div>
+</div>
+</div>
+<div id="template-i2mkhh" className="gjs-row">
+<div id="template-i8ymtx" className="gjs-cell">
+<div id="template-izjin5" className="gjs-row">
+<div id="template-i4ai3b" className="gjs-cell">
+<div id="template-itr1db" className="">
+10Decoders Books For Free Now
+              </div>
+<div id="template-iae45j" className="">
+10Decoders books simplifies invoicing, billing, and accounting for 
+              </div>
+<div id="template-i1m5vn" className="">
+freelancers and small business owners.
+              </div>
+</div>
+</div>
+<div id="template-i1znre" className="gjs-row">
+<div id="template-ilpjas" className="gjs-cell">
+<button id="template-i4d7h6" className="button btn btn-primary ">
+Login</button>
+<button id="template-ixhf85" className="button btn btn-primary ">
+Signup</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+ 
             </>
           );
         };
